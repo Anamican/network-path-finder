4ebb493f2c3e9a9b5db80f93997e8c7b9f16fe19
Madhu <madhukumarit23@gmail.com>
2020-10-29 13:25:59 +1100

    maintain standard

--- tests/SubscribeHR/Parser/FileParserTest.php
+++ tests/SubscribeHR/Parser/FileParserTest.php
@@ -13,24 +13,24 @@ class FileParserTest extends TestCase
     public function testCheckEmptyFilePath(){
         $fParser = new FileParser();
         $isReadable = $fParser->isFileReadable('');
-        self::assertFalse($isReadable);
+        $this->assertFalse($isReadable);
     }
 
     public function testCheckIncorrectFilePath(){
         $fParser = new FileParser();
         $isReadable = $fParser->isFileReadable('some-random-garbase-ajfjfwebfw-file.csv');
-        self::assertFalse($isReadable);
+        $this->assertFalse($isReadable);
     }
 
     public function testCheckEmptyFile(){
         $fParser = new FileParser();
         $isReadable = $fParser->isFileReadable(self::TEST_FILES_DIR.'empty_file.csv');
-        self::assertFalse($isReadable);
+        $this->assertFalse($isReadable);
     }
 
     public function testCheckNonEmptyFile(){
         $fParser = new FileParser();
         $isReadable = $fParser->isFileReadable(self::TEST_FILES_DIR.'non_empty_file.csv');
-        self::assertTrue($isReadable);
+        $this->assertTrue($isReadable);
     }
 }
